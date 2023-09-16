2dc6e38825762ab8cdea95f5b9280367168cf95f
williamasjr <wasjr00@gmail.com>
2023-09-15 22:18:21 -0300

    feat: add dark mode theme and button to toggle it

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle"
 import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <div>
       <UserButton
-      afterSignOutUrl="/" />
+      afterSignOutUrl="/"
+      />
+      <ModeToggle/>
     </div>
   )
 }
