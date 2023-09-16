fedac9791b46e0bac93541a2a19c75b157bf90eb
williamasjr <wasjr00@gmail.com>
2023-09-15 21:07:14 -0300

    feat: create a initial organization for a folders

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode } ) => {
+    return ( 
+        <div className="">
+            {children}
+        </div>
+     );
+}
+ 
+export default AuthLayout;
