a5a049f797cca40a7991fe85291c4fa475f1ed83
williamasjr <wasjr00@gmail.com>
2023-09-20 23:04:19 -0300

    feat: create leave and delete server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -94,6 +94,7 @@ export const ServerHeader = ({
         )}
         {isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Delete Server
@@ -104,6 +105,7 @@ export const ServerHeader = ({
         )}
         {!isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Leave Server
